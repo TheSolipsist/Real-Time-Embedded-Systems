5cceb8d83790526bef979657b0406bbbc6a2e1d2
TheSolipsist <57457393+TheSolipsist@users.noreply.github.com>
2021-11-21 16:11:58 +0200

    Changes

--- Final_Assignment/threads.c
+++ Final_Assignment/threads.c
@@ -3,8 +3,13 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <time.h>
+#include <inttypes.h>
 
+// N_MACADDRESSES should be less than 2^16
+// If a larger number is required, change the data type of random_index in BTnearMe()
 #define N_MACADDRESSES 2000
+#define SEARCH_INTERVAL 10
 
 struct macaddress
 {
@@ -14,75 +19,92 @@ struct macaddress
 static struct macaddress recent_macaddress_list[24];
 static struct macaddress macaddress_list[N_MACADDRESSES];
 
-uint64_t lfsr_get_next(uint64_t lfsr_seed, uint8_t bits_to_shift)
-{
-    /*
-    LFSR implementation for pseudo-random number generation
-    Feedback function: XOR (output bit, next bit)
-    Arguments:
-        uint64_t lfsr_seed: the seed used for the generation of the next random number
-        uint8_t bits_to_shift: the number of bits to shift left (max: 64)
-    */
-
-    uint64_t lfsr_next;
-
-    uint8_t output_bit = lfsr_seed & 1;
-    lfsr_next = lfsr_seed >> 1;
-    lfsr_next += (output_bit ^ (lfsr_next & 1)) << bits_to_shift;
-
-    return lfsr_next;
-}
-
 void init_macaddress_list()
 {
-    // Randomly initialize the seed
-    uint64_t seed = 38483;
-    // For 48-bit random numbers, the LFSR needs to shift the feedback bit 47 times to the left
-    uint8_t bits_to_shift = 47;
+    // Initialize macaddress_list by filling it with random macaddresses
     for (int i = 0; i < N_MACADDRESSES; ++i)
     {
-        seed = lfsr_get_next(seed, 47);
-        macaddress_list[i].value = seed;
+        macaddress_list[i].value = 0;
+        for (int byte_i = 0; byte_i < 6; ++byte_i)
+        {
+            // Getting a random byte, since we don't know the limitations of RAND_MAX (minimum supported should be 0x7FFF)
+            uint8_t rand_byte = rand();
+            // The random byte is placed on its correct position (48-bit macaddress has 6 bytes)
+            macaddress_list[i].value += rand_byte << (byte_i * 8);
+            // Based on this approach, the probability of getting the same macaddress twice is negligible for the sample
+            // size defined by N_MACADDRESSES (for 2000, the probability is around 10^-9)
+        }
     }
 }
 
-static uint64_t btnearme_seed = 138921;
-// struct macaddress BTnearMe()
-uint8_t BTnearMe()
+struct macaddress BTnearMe()
 {
     // Get a random macaddress from macaddress_list
-    btnearme_seed = lfsr_get_next(btnearme_seed, 63);
-    uint8_t random_index = btnearme_seed % N_MACADDRESSES;
-    // return macaddress_list[random_index];
-    return random_index;
+    uint16_t random_index = rand() % N_MACADDRESSES;
+    return macaddress_list[random_index];
 }
+
+// -------------------------------
+// USED FOR TESTING PURPOSES ONLY!
+void test_rand()
+{
+    uint16_t random_index;
+    uint64_t occurences[N_MACADDRESSES] = {0};
+    for (uint64_t i = 0; i < 100000000; ++i)
+    {
+        random_index = rand() % N_MACADDRESSES;
+        occurences[random_index] += 1;
+    }
+    FILE *data;
+    data = fopen("occurences.txt", "w");
+    if (data == NULL)
+    {
+        fprintf(stderr, "Error opening file\n");
+        exit(1);
+    }
+    printf("Writing occurences...\n");
+    for (uint16_t i = 0; i < N_MACADDRESSES; ++i)
+    {
+        fprintf(data, "%" PRIu64 "\n", occurences[i]);
+    }
+    fclose(data);
+}
+
+void test_init()
+{
+    uint64_t NUM_TESTS = 10000;
+    for (uint64_t i = 0; i < NUM_TESTS; ++i)
+    {
+        for (int j = 0; j < N_MACADDRESSES; ++j)
+        {
+            macaddress_list[j].value = 0;
+        }
+        init_macaddress_list();
+        for (int j = 0; j < N_MACADDRESSES; ++j)
+        {
+            for (int z = j + 1; z < N_MACADDRESSES; ++z)
+            {
+                if (macaddress_list[j].value == macaddress_list[z].value)
+                {
+                    printf("Houston, we got a problem.\n");
+                }
+            }
+        }
+    }
+}
+// TESTING ENDS HERE!
+// -------------------------------
+
 
 int main()
 {
+    srand(time(NULL));
+
     pthread_t mac_search, covid_test;
 
-    init_macaddress_list();
-    // for (int i = 0; i < N_MACADDRESSES; ++i)
-    // {
-    //     for (int j = i + 1; j < N_MACADDRESSES; ++j)
-    //     {
-    //         if (macaddress_list[i].value == macaddress_list[j].value)
-    //         {
-    //             printf("Problem!!\n");
-    //         }
-    //     }
-    // }
+    // init_macaddress_list();
 
-    long int occurances[N_MACADDRESSES] = {0};
-    uint8_t random_num;
-    for (int i = 0; i < 100000; ++i)
-    {
-        random_num = BTnearMe();
-        occurances[random_num] += 1;
-    }
-    for (int i = 0; i < N_MACADDRESSES; ++i)
-    {
-        printf("%ld\n", occurances[i]);
-    }
+    test_init();
+
     return 0;
 }
