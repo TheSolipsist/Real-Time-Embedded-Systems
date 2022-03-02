4c1bf9c4ba3d4196908eddb014c2c32c52d1bcb6
TheSolipsist <57457393+TheSolipsist@users.noreply.github.com>
2022-03-03 00:55:22 +0200

    changes

--- Final_Assignment/main.c
+++ Final_Assignment/main.c
@@ -5,34 +5,57 @@
 #include <unistd.h>
 #include <time.h>
 #include <inttypes.h>
+#include <stdbool.h>
 
-#define N_MACADDRESSES 2000 // If a much larger number is required, check the data type of random_index in BTnearMe()
+#define N_MACADDRESSES 200 // If a much larger number is required, check the data type of random_index in BTnearMe()
+
+struct timespec seconds_to_timespec(float seconds)
+{
+    struct timespec result;
+    result.tv_sec = (int) seconds;
+    result.tv_nsec = (int)(1E9 * (seconds - result.tv_sec));
+    return result;
+}
 
 // Time interval parameters
-const double speed_factor = 0.01; // 0.01 means the code will run 100x faster
-const double search_interval = 10 * speed_factor;
-const double save_recent = 4 * 60 * speed_factor;
-const double forget_recent = 16 * 60 * speed_factor;
-const double forget_contacts = 14 * 24 * 60 * 60 * speed_factor;
-const double test_interval = 4 * 60 * 60 * speed_factor;
+const double speed_factor = 0.01; // The code will run (1 / speed_factor) times faster (0.01 -> 100x)
+const double sec_search_interval = 10 * speed_factor;
+const double sec_save_recent = 4 * 60 * speed_factor;
+const double sec_forget_recent = 16 * 60 * speed_factor;
+const double sec_forget_contacts = 14 * 24 * 60 * 60 * speed_factor;
+const double sec_test_interval = 4 * 60 * 60 * speed_factor;
+const double sec_program_timeout = 30 * 24 * 60 * 60 * speed_factor;
 
-struct macaddress
-{
-    uint64_t value : 48;
-};
+// struct timespec variables that correspond to the above parameters, initialized in main()
+struct timespec search_interval;
+struct timespec save_recent;
+struct timespec forget_recent;
+struct timespec forget_contacts;
+struct timespec test_interval;
+struct timespec program_timeout;
 
 typedef struct
 {
-    struct macaddress *buffer;
-    uint16_t head, tail, size;
+    uint64_t value : 48;
+} macaddress;
 
+typedef struct
+{
+    macaddress *buffer;
+    uint16_t head, tail, size;
 } queue;
 
-static struct macaddress macaddress_list[N_MACADDRESSES];
-static queue *recent_contacts;
-static queue *close_contacts;
-static pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
-static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+macaddress macaddress_list[N_MACADDRESSES];
+queue *recent_contacts;
+queue *close_contacts; 
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t program_timeout_cond = PTHREAD_COND_INITIALIZER;
+
+struct timespec program_start_time;
+struct timespec next_BTnearMe_time;
+struct timespec next_testCovid_time;
+struct timespec next_;
+
 
 void init_macaddress_list()
 {
@@ -44,22 +67,56 @@ void init_macaddress_list()
     }
 }
 
-struct macaddress BTnearMe()
+bool testCovid()
+{
+}
+
+void *BTnearMe_thread_func(void *args)
+{
+    struct timespec next_BTnearMe_time = {.tv_sec = 0, .tv_nsec = 0};
+    pthread_mutex_lock(&mut);
+    while (1)
+    {
+        // timespecadd()
+    }
+}
+
+macaddress BTnearMe()
 {
     // Get a random macaddress from macaddress_list
     uint16_t random_index = rand() % N_MACADDRESSES;
     return macaddress_list[random_index];
 }
 
+void timespec_add(struct timespec *a, struct timespec *b, struct timespec *result) {
+    result->tv_sec = a->tv_sec + b->tv_sec;
+    result->tv_nsec = a->tv_nsec + b->tv_nsec;
+    if (result->tv_nsec > 999999999)
+    {
+        ++(result->tv_sec);
+        result->tv_nsec -= 1000000000L;
+    }
+}
+
 int main()
 {
     srand(time(NULL));
 
-    pthread_t mac_search;
-    pthread_t covid_test;
+    search_interval = seconds_to_timespec(sec_search_interval);
+    save_recent = seconds_to_timespec(sec_save_recent);
+    forget_recent = seconds_to_timespec(sec_forget_recent);
+    forget_contacts = seconds_to_timespec(sec_forget_contacts);
+    test_interval = seconds_to_timespec(sec_test_interval);
+    program_timeout = seconds_to_timespec(sec_program_timeout);
 
+    pthread_t BTnearMe_t;
+    pthread_t testCovid_t;
+    pthread_t uploadContacts_t;
 
     init_macaddress_list();
+    clock_gettime(CLOCK_MONOTONIC, &program_start_time);
+
+    printf("%jd.%09ld\n", (intmax_t)search_interval.tv_sec, search_interval.tv_nsec);
 
     return 0;
 }
