51afe4592b178545b228434618ffd0044d1715e0
TheSolipsist <57457393+TheSolipsist@users.noreply.github.com>
2022-03-05 04:22:18 +0200

    changes

--- Final_Assignment/main.c
+++ Final_Assignment/main.c
@@ -7,7 +7,11 @@
 #include <inttypes.h>
 #include <stdbool.h>
 
-#define N_MACADDRESSES 200 // If a much larger number is required, check the data type of random_index in BTnearMe()
+#define N_MACADDRESSES 2000 // If a much larger number is required, check the data type of random_index in BTnearMe()
+#define MAX_CONCURRENT_BTNEARME 5000 // Max number of possible concurrent threads for new macaddresses
+#define MAX_CLOSE_CONTACTS 5000
+#define MAX_BTNEARME_CALLS 300000
+#define MAX_TESTCOVID_CALLS 500
 
 struct timespec seconds_to_timespec(float seconds)
 {
@@ -45,17 +49,56 @@ typedef struct
     uint16_t head, tail, size;
 } queue;
 
+typedef struct
+{
+    macaddress generated_macaddress; 
+    struct timespec macaddress_time;
+} btnearme_history_struct;
+
+typedef struct
+{
+    bool testCovidResult;
+    struct timespec testCovid_time;
+} testCovid_history_struct;
+
+typedef struct
+{
+    pthread_t pt_arr[MAX_CONCURRENT_BTNEARME];
+    uint16_t head, tail;
+    bool empty;
+} pthread_struct;
+
 macaddress macaddress_list[N_MACADDRESSES];
-queue *recent_contacts;
-queue *close_contacts; 
-pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+btnearme_history_struct btnearme_history[MAX_BTNEARME_CALLS];
+testCovid_history_struct testCovid_history[MAX_TESTCOVID_CALLS];
+pthread_struct pthreads_new_macaddress[MAX_CONCURRENT_BTNEARME];
+// recent_contacts holds the number of times that each macaddress has been seen 4-20 minutes ago
+uint16_t recent_contacts[N_MACADDRESSES] = {0};
+bool close_contacts[N_MACADDRESSES];
+
+pthread_mutex_t contacts_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t orchestrator_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 pthread_cond_t program_timeout_cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t new_macaddress_cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t update_contacts_started_cond = PTHREAD_COND_INITIALIZER;
+
+bool program_ended = false;
+bool update_contacts_started = false;
 
 struct timespec program_start_time;
 struct timespec next_BTnearMe_time;
 struct timespec next_testCovid_time;
-struct timespec next_;
 
+void timespec_add(struct timespec *a, struct timespec *b, struct timespec *result) {
+    result->tv_sec = a->tv_sec + b->tv_sec;
+    result->tv_nsec = a->tv_nsec + b->tv_nsec;
+    if (result->tv_nsec > 999999999)
+    {
+        ++(result->tv_sec);
+        result->tv_nsec -= 1000000000L;
+    }
+}
 
 void init_macaddress_list()
 {
@@ -67,20 +110,6 @@ void init_macaddress_list()
     }
 }
 
-bool testCovid()
-{
-}
-
-void *BTnearMe_thread_func(void *args)
-{
-    struct timespec next_BTnearMe_time = {.tv_sec = 0, .tv_nsec = 0};
-    pthread_mutex_lock(&mut);
-    while (1)
-    {
-        // timespecadd()
-    }
-}
-
 macaddress BTnearMe()
 {
     // Get a random macaddress from macaddress_list
@@ -88,13 +117,46 @@ macaddress BTnearMe()
     return macaddress_list[random_index];
 }
 
-void timespec_add(struct timespec *a, struct timespec *b, struct timespec *result) {
-    result->tv_sec = a->tv_sec + b->tv_sec;
-    result->tv_nsec = a->tv_nsec + b->tv_nsec;
-    if (result->tv_nsec > 999999999)
+bool testCovid()
+{
+}
+
+
+void *update_contacts_func()
+{
+    pthread_mutex_lock(&contacts_mutex);
+    while (1)
     {
-        ++(result->tv_sec);
-        result->tv_nsec -= 1000000000L;
+        update_contacts_started = true;
+        pthread_cond_signal(&update_contacts_started_cond);
+        pthread_cond_wait(&new_macaddress_cond, &contacts_mutex);
+    }
+}
+
+void *orchestrator_new_macaddress_thread_func()
+{
+
+}
+
+void *BTnearMe_thread_func()
+{
+    // Run 0 should occur immediately, so next_BTnearMe_time is initialized as program_start_time
+    struct timespec next_BTnearMe_time = program_start_time;
+    macaddress new_macaddress;
+    if (!update_contacts_started)
+    {
+        pthread_cond_wait(&update_contacts_started_cond, &contacts_mutex);
+    }
+    pthread_mutex_lock(&contacts_mutex);
+    while (1)
+    {
+        // thread waits until program shutdown signal or the search interval has passed
+        pthread_cond_timedwait(&program_timeout_cond, &contacts_mutex, &next_BTnearMe_time);
+        new_macaddress = BTnearMe();
+        pthread_cond_signal(&new_macaddress_cond);
+        timespec_add(&search_interval, &next_BTnearMe_time, &next_BTnearMe_time);
+
+
     }
 }
 
@@ -112,6 +174,7 @@ int main()
     pthread_t BTnearMe_t;
     pthread_t testCovid_t;
     pthread_t uploadContacts_t;
+    pthread_t update_contacts_t;
 
     init_macaddress_list();
     clock_gettime(CLOCK_MONOTONIC, &program_start_time);
